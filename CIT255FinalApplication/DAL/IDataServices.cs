83656b5fa68f856a547a43b38709f2b4e0da05e3
Chris Cosnowski <chris.cosnowski@gmail.com>
2018-12-08 02:27:26 -0500

    Seventh Commit

---
+++ CIT255FinalApplication/DAL/IDataServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIT255FinalApplication
+{
+    public interface IDataServices
+    {
+        List<ListOfItem> ReadAll();
+        void WriteAll(List<ListOfItem> listOfItems);
+    }
+}
